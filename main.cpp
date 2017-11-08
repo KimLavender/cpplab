86820666b541c44d542855366d074d80dcfc6873
lavender <3142735960@qq.com>
2017-11-08 15:46:10 +0800

    3.6

--- main.cpp
+++ main.cpp
@@ -6,7 +6,37 @@
 
 using namespace std;
 
-int main() {
-    cout << "hello";
-    return 0;
+class Student {
+public:
+    int number;
+    int score;
+
+    Student(int Number, int Score) : number(Number), score(Score) {}
+};
+
+Student student[5] = {
+        Student(001, 85),
+        Student(002, 93),
+        Student(003, 81),
+        Student(004, 79),
+        Student(005, 88)
+};
+
+Student max();
+
+
+int main(){
+    cout << "The greatest student's number is " << max().number << endl;
+}
+
+Student max(){
+    Student temp = student[0];
+    for(int i = 1; i <5; i++){
+        if(temp.score > student[i].score){
+            continue;
+        }
+        else {
+            temp = student[i]; }
+    }
+    return temp;
 }
